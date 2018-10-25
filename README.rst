249e046424dc3d1dbf77b3099aef337b965a3e5d
Adam Tengler <atengler@mirantis.com>
2018-10-25 22:20:49 +0200

    Parametrize init container

--- README.rst
+++ README.rst
@@ -1,2 +1,10 @@
 CockroachDB init container
 ==========================
+
+Initialize CockroachDB with database and user with ALL grants on it
+
+Environment:
+
+  - COCKROACH_HOST: <cockroachdb-hostname>
+  - DB_NAME: <database-name>
+  - DB_USER: <username>
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 SQL="/cockroach/cockroach.sh sql --insecure --host ${COCKROACH_HOST} -e"
-PRESENT=$(${SQL} "SELECT * FROM system.users WHERE username='oapi';" | grep "oapi")
+PRESENT=$(${SQL} "SELECT * FROM system.users WHERE username='${DB_USER}';" | grep "$DB_USER")
 
-echo "CREATE DATABASE IF NOT EXISTS oapi"
-$SQL "CREATE DATABASE IF NOT EXISTS oapi;"
+echo "CREATE DATABASE IF NOT EXISTS $DB_NAME"
+$SQL "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 if [ -z $PRESENT ]; then
-    echo "CREATE USER oapi"
-    $SQL "CREATE USER oapi;"
-    echo "GRANT ALL ON DATABASE oapi TO oapi"
-    $SQL "GRANT ALL ON DATABASE oapi TO oapi;"
+    echo "CREATE USER $DB_USER"
+    $SQL "CREATE USER $DB_USER;"
+    echo "GRANT ALL ON DATABASE $DB_USER TO $DB_USER"
+    $SQL "GRANT ALL ON DATABASE $DB_USER TO $DB_USER;"
 fi
